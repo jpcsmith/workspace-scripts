9af7ec634ab3dae847897d676675c66f9f1ef57f
Jean-Pierre Smith <jpc.smith@yahoo.com>
2016-06-17 20:21:09 +0200

    Made apt-get quieter.

--- workspace_setup.sh
+++ workspace_setup.sh
@@ -1,20 +1,22 @@
 #! /bin/bash
 
+
 # ---
 # Display the SSH public key to be added to github
 #
 # Generates a new key if it does not exist.
 setup_github() {
-    sudo apt-get install xclip
+    sudo apt-get -qq install xclip
     # Make the .ssh directory if it doesnt exist
     if [[ ! -f ~/.ssh/id_rsa ]]; then
         ssh-keygen -t rsa -b 4096 -C "jpc.smith@yahoo.com" \
             -f ~/.ssh/id_rsa \
             -N ""
+        ssh-add ~/.ssh/id_rsa
     fi
-    ssh-add ~/.ssh/id_rsa
     xclip -sel clip < ~/.ssh/id_rsa.pub
     echo "Add following ~/.ssh/id_rsa.pub to github, its on the clipboard."
+    echo "---"
     cat ~/.ssh/id_rsa.pub
 }
 
@@ -23,7 +25,7 @@ setup_github() {
 # Download the solarized theme for gnome-terminal
 #
 setup_solarized() {
-    sudo apt-get install dconf-cli
+    sudo apt-get -qq install dconf-cli
     git clone \
         https://github.com/Anthony25/gnome-terminal-colors-solarized.git \
         /tmp/solarized
@@ -40,11 +42,11 @@ setup_vim() {
     nvim_dir=~/.config/nvim
     # Install NeoVIM
     sudo add-apt-repository ppa:neovim-ppa/unstable
-    sudo apt-get update
-    sudo apt-get install neovim
+    sudo apt-get -qq update
+    sudo apt-get -qq install neovim
     sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
     # Link the config file
-    ln -f -s $PWD/resources/init.vim ${nvim_dir}/init.vim
+    ln -f -s -t ${nvim_dir} $PWD/resources/init.vim || exit 1
     # Download and install the package manager
     mkdir ${nvim_dir}/dein
     curl ${dein_github} > ${install_script}
@@ -52,11 +54,13 @@ setup_vim() {
     # Install the fonts used for Airline
     git clone https://github.com/powerline/fonts /tmp/powerline_fonts
     pushd /tmp/powerline_fonts
-    sh ./install.sh
+    ./install.sh
     popd
 
+    echo "------------------------------------------------------------------"
     echo "Please set the font (Edit > Profile Preferences) to:"
     echo "  \"DejaVu Sans Mono for Powerline Book, 11.5\""
+    echo "And checkout the "linter" repo in jedi once plugins are installed."
 }
 
 
@@ -68,9 +72,9 @@ setup_vim() {
     exit 1; 
 }
 # General setup
-sudo apt-get install software-properties-common
+sudo apt-get -qq install software-properties-common
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-sudo apt-get update
+sudo apt-get -qq update
 
 # Run the command
 case "$1" in 
